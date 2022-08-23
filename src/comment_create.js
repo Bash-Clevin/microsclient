e07e3cd917e6caf05478cb98f13037f7bfbeb8bc
kevin otieno <kevin.otieno@megatherium.solutions>
2022-08-23 07:39:40 +0300

    fix: comments not displaying textfield

--- src/comment_create.js
+++ src/comment_create.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 export default ({ postId }) => {
-    const [ content, steContent ] = useState('');
+    const [ content, setContent ] = useState('');
 
     const onSubmit = async (event) => {
         event.preventDefault(); 
@@ -11,20 +11,21 @@ export default ({ postId }) => {
             content
         });
 
-        steContent('');
+        setContent('');
     };
 
-    return 
-    <div>
-        <form onSubmit={onSubmit}>
-            <div className="form-group">
-                <label>New Comment</label>
-                <input 
-                    value={content} 
-                    onChange={e => steContent(e.target.value)}
-                    className="form-control"/>                    
-            </div>
-            <button className="btn btn-primary">Submit</button>
-        </form>
-    </div>;
+    return( 
+        <div>
+            <form onSubmit={onSubmit}>
+                <div className="form-group">
+                    <label>New Comment</label>
+                    <input 
+                        value={content} 
+                        onChange={e => setContent(e.target.value)}
+                        className="form-control"/>                    
+                </div>
+                <button className="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    );
 };
